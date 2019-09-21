3dbbf67a931b71a3f7559ae0929adf1443467381
warrenm16 <warrenm16@cs104u-6d.acad.fullerton.edu>
2019-09-21 15:31:04 -0700

    prob03 complete

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,23 @@
 // Sales prediction
+
+
+#include <iostream>
+
+int main()
+{
+  //
+   const long long LAST_YEARS_SALE = 2103419277;
+   const double increase = 0.18;
+  long long this_years_sales;
+
+//Formula for sales prediction 
+this_years_sales = (LAST_YEARS_SALE * increase) + LAST_YEARS_SALE ;
+
+  // Displays last year sales and sales prediction
+  std::cout << "Last year\'s sales were $" << LAST_YEARS_SALE << std::endl;
+  std::cout << "This year\'s sales prediction: $" << this_years_sales << std::endl;
+
+
+
+return 0;
+}
